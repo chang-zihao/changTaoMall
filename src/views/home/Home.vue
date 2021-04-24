d52f1a90f9e0200d4393c7cc671f4dd33d0fa768
chang <769846892@qq.com>
2021-04-24 23:33:41 +0800

    完成了轮播图下面的推荐信息，就是4个图片那个。完成了本周流行（featureView），完成了TabControl组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,22 +1,84 @@
 <template>
-  <div id="Home">
+  <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control
+      class="tab-control"
+      :title="['流行', '新款', '精选']"
+    ></tab-control>
+
+    <ul>
+      <li>111111111</li>
+      <li>22222222</li>
+      <li>3333333</li>
+      <li>43444444</li>
+      <li>555555555</li>
+      <li>666666666</li>
+      <li>777777777</li>
+      <li>88888888</li>
+      <li>99999999</li>
+      <li>111111111</li>
+      <li>22222222</li>
+      <li>3333333</li>
+      <li>43444444</li>
+      <li>555555555</li>
+      <li>666666666</li>
+      <li>777777777</li>
+      <li>88888888</li>
+      <li>99999999</li>
+      <li>111111111</li>
+      <li>22222222</li>
+      <li>3333333</li>
+      <li>43444444</li>
+      <li>555555555</li>
+      <li>666666666</li>
+      <li>777777777</li>
+      <li>88888888</li>
+      <li>99999999</li>
+      <li>111111111</li>
+      <li>22222222</li>
+      <li>3333333</li>
+      <li>43444444</li>
+      <li>555555555</li>
+      <li>666666666</li>
+      <li>777777777</li>
+      <li>88888888</li>
+      <li>99999999</li>
+      <li>111111111</li>
+      <li>22222222</li>
+      <li>3333333</li>
+      <li>43444444</li>
+      <li>555555555</li>
+      <li>666666666</li>
+      <li>777777777</li>
+      <li>88888888</li>
+      <li>99999999</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabcontrol/TabControl";
 
 import { getHomeMultidata } from "network/home";
 
 export default {
   name: "Home",
   components: {
-    NavBar,
     HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
   },
+
   data() {
     return {
       banners: [],
@@ -34,8 +96,20 @@ export default {
 };
 </script>
 <style>
+#home {
+  padding-top: 44px;
+}
 .home-nav {
+  position: fixed;
+  right: 0;
+  left: 0;
+  top: 0;
+  z-index: 9;
   background-color: #ff8198;
   color: #fff;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="feature">
+    <a href="https://www.mogu.com/"
+      ><img src="~assets/img/home/recommend_bg.jpg" alt=""
+    /></a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.feature a img {
+  width: 100%;
+}
+</style>
